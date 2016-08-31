931ec9ac49dc8b1c1360e638c3838bb5abd9deef
Leandro Di Tommaso <leandro.ditommaso@mikroways.net>
2016-08-31 12:16:54 -0300

    Entrypoint consider restarts.
    * Each time the container was started a new secret was being generated.
    * User can now set the secret.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,23 @@
 
 set -e
 
-# Generate aleatory secret to be used for posts requests.
-SECRET=`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-64};echo;`
-echo "Secret to send posts with: $SECRET"
+function startup {
+  # If SECRET is not set, generate random string to be used for posts requests.
+  : ${SECRET=`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-64};echo;`}
+  echo "Secret to send posts with: $SECRET"
 
-# Replaces REPO_URL for the one passed as an environment variable.
-sed -i 's@REPO_URL@'"$REPO_URL"'@g' /bin/generate-html.sh
-# Replaces SECRET expected in posts requests for the randomly generated one.
-sed -i 's@SECRET@'"$SECRET"'@g' /bin/generate-html.sh
+  # Replaces REPO_URL for the one passed as an environment variable.
+  sed -i 's@REPO_URL@'"$REPO_URL"'@g' /bin/generate-html.sh
+  # Replaces SECRET expected in posts requests for the randomly generated one.
+  sed -i 's@SECRET@'"$SECRET"'@g' /bin/generate-html.sh
 
-echo "www-data ALL=(root) NOPASSWD: /bin/generate-html.sh" >> /etc/sudoers
-htpasswd -bc /etc/nginx/.htpasswd $HTTP_USER $HTTP_PASS
-/bin/generate-html.sh $SECRET
+  echo "www-data ALL=(root) NOPASSWD: /bin/generate-html.sh" >> /etc/sudoers
+  htpasswd -bc /etc/nginx/.htpasswd $HTTP_USER $HTTP_PASS
+  /bin/generate-html.sh $SECRET
+}
+
+# The following line executes startup function the first time the
+# container is started.
+grep -q REPO_URL /bin/generate-html.sh && startup
+
 /usr/sbin/nginx -g "daemon off; error_log /dev/stdout;"
